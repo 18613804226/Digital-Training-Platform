27abd8839934fe35214143d9da56c7f9b8c839ca
18613804226 <5538072662@qq.com>
2025-11-30 17:45:48 +0300

    fix: wujie mistake

--- apps/web-ele/src/views/microFrontendZone/reactMaster/index.vue
+++ apps/web-ele/src/views/microFrontendZone/reactMaster/index.vue
@@ -6,7 +6,7 @@ import WujieVue from 'wujie-vue3';
 // console.log(import.meta.env.VITE_REACT_SUB_APP_ADDRESS);
 
 const subAppUrl = import.meta.env.VITE_REACT_SUB_APP_ADDRESS; // React 子应用开发地址
-const loading = ref(true);
+
 
 const handleBeforeLoad = () => {
   console.log('子应用开始加载');
@@ -20,13 +20,7 @@ const handleAfterLoad = () => {
 
 <template>
   <div class="micro-app-container">
-    <WujieVue
-      width="100%"
-      height="100%"
-      name="react-course"
-      :url="subAppUrl"
-      :sync="true"
-    />
+    <WujieVue width="100%" height="100%" name="react-course" :url="subAppUrl" :sync="true" />
   </div>
 </template>
 
